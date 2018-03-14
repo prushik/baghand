39642d293a7fdea90583fc2ef247f5f36264d550
Philip Rushik <prushik@gmail.com>
2018-03-13 23:28:33 -0400

    make command line arguments work

--- baghand.c
+++ baghand.c
@@ -120,6 +120,92 @@ struct zip_eocd
 	unsigned char *comment;
 };
 
+struct deflate_store_header
+{
+	uint8_t method;
+	uint16_t block_size;
+	uint16_t inverse_size;
+}__attribute__((packed));
+
+#define BH_MODE_MAKE_TAR 'c'
+#define BH_MODE_MAKE_TGZ 'z'
+#define BH_MODE_EXTRACT  'x'
+
+void usage()
+{
+	write(1, "Usage:\n", 7);
+	write(1, "\tbaghand [options] <zip file> <tar file>\n", 41);
+	write(1, "\n", 1);
+	write(1, "Options:\n", 9);
+	write(1, "\t-c \t tar mode. Create a tarball of gzipped files. [default]\n", 61);
+	write(1, "\t-t \t tar.gz mode. Create a gzipped tarball.\n", 45);
+	write(1, "\t-x \t extract mode. Extract the files to gzipped files.\n", 56);
+}
+
+
+// -------------------RFC1952 CRC code--------------------------
+
+/* Table of CRCs of all 8-bit messages. */
+unsigned long crc_table[256];
+
+/* Flag: has the table been computed? Initially false. */
+int crc_table_computed = 0;
+
+/* Make the table for a fast CRC. */
+void make_crc_table(void)
+{
+	unsigned long c;
+	int n, k;
+	for (n = 0; n < 256; n++) {
+		  c = (unsigned long) n;
+		  for (k = 0; k < 8; k++) {
+		if (c & 1) {
+		  c = 0xedb88320L ^ (c >> 1);
+		} else {
+		  c = c >> 1;
+		}
+	  }
+	  crc_table[n] = c;
+	}
+	crc_table_computed = 1;
+}
+
+/*
+ Update a running crc with the bytes buf[0..len-1] and return
+the updated crc. The crc should be initialized to zero. Pre- and
+post-conditioning (one's complement) is performed within this
+function so it shouldn't be done by the caller. Usage example:
+
+ unsigned long crc = 0L;
+
+ while (read_buffer(buffer, length) != EOF) {
+   crc = update_crc(crc, buffer, length);
+ }
+ if (crc != original_crc) error();
+*/
+unsigned long update_crc(uint32_t crc,
+			  unsigned char *buf, int len)
+{
+	uint32_t c = crc ^ 0xffffffffL;
+	int n;
+
+	if (!crc_table_computed)
+		make_crc_table();
+	for (n = 0; n < len; n++) {
+		c = crc_table[(c ^ buf[n]) & 0xff] ^ (c >> 8);
+	}
+	return c ^ 0xffffffffL;
+}
+
+/* Return the CRC of the bytes buf[0..len-1]. */
+inline uint32_t crc(unsigned char *buf, int len)
+{
+	return update_crc(0L, buf, len);
+}
+// -------------------RFC1952 CRC code end----------------------
+
+
+
 // this function checks the zip magic and the comment length to see if 
 // the eocd structure has been located
 inline int validate_eocd(struct zip_eocd *eocd, uint32_t offset)
@@ -135,6 +221,7 @@ void octal(int n, unsigned char *buffer, int len)
 		buffer[len-1-i] = (n & 7) + '0';
 }
 
+// This is used to make sure that the end of each tar entry is aligned to 512 bytes
 static const unsigned char padding[512] = {0};
 
 void tar_set_checksum(struct tar_posix_header *header)
@@ -165,6 +252,85 @@ void tar_write(unsigned char *fname, int zip_fd, int tar_fd, struct zip_director
 	struct gz_header header = {0};
 	struct gz_footer footer = {0};
 	int i;
+	buffer = malloc(dir_entry->zip_size);
+
+	// tar headers
+	if (dir_entry->fname_len < 98)
+		for (i=0; fname[i] != 0; i++)
+				tar_header.name[i] = fname[i];
+	else
+		for (i=0; fname[i] != 0; i++)
+			if (i < 155)
+				tar_header.prefix[i] = fname[i];
+			else
+				tar_header.name[i-155] = fname[i];
+
+	for (i=0; i < 7; i++)
+	{
+		tar_header.mode[i] = '0';
+		tar_header.gid[i] = '0';
+		tar_header.uid[i] = '0';
+		tar_header.mtime[i] = '0';
+	}
+	tar_header.typeflag = '0';
+
+	octal(tar_entry_size(dir_entry->zip_size, dir_entry->compression == ZIP_ALG_DEFLATE), tar_header.size, 11);
+
+	tar_header.magic[0] = 'u';
+	tar_header.magic[1] = 's';
+	tar_header.magic[2] = 't';
+	tar_header.magic[3] = 'a';
+	tar_header.magic[4] = 'r';
+	tar_header.magic[5] = ' ';
+	tar_header.magic[6] = ' ';
+//	tar_header.version[0] = '0';
+//	tar_header.version[1] = '0';
+
+	tar_set_checksum(&tar_header);
+
+	// gz headers
+	header.magic = GZ_MAGIC;
+	header.method = GZ_METHOD_DEFLATE;
+	header.flags = 0;
+	header.os = GZ_OS_LINUX;
+
+	footer.crc = dir_entry->crc32;
+	footer.isize = dir_entry->unzip_size;
+
+	// jump to the file and extract it
+	int pos = lseek(zip_fd, 0, SEEK_CUR);
+	lseek(zip_fd, dir_entry->offset, SEEK_SET);
+
+	read(zip_fd, &file_entry, 30);
+	lseek(zip_fd, file_entry.fname_len + file_entry.extra_len, SEEK_CUR);
+	read(zip_fd, buffer, dir_entry->zip_size);
+
+	write(tar_fd, &tar_header, sizeof(struct tar_posix_header));
+	if (dir_entry->compression == ZIP_ALG_DEFLATE)
+	{
+		write(tar_fd, &header, sizeof(struct gz_header));
+		write(tar_fd, buffer, dir_entry->zip_size);
+		write(tar_fd, &footer, sizeof(struct gz_footer));
+	}
+	else if (dir_entry->compression == ZIP_ALG_STORE)
+	{
+		write(tar_fd, buffer, dir_entry->zip_size);
+	}
+	write(tar_fd, padding, 512 - ((sizeof(struct tar_posix_header) + tar_entry_size(dir_entry->zip_size, dir_entry->compression == ZIP_ALG_DEFLATE)) % 512));
+
+	free(buffer);
+
+	lseek(zip_fd, pos, SEEK_SET);
+}
+
+void tgz_write(unsigned char *fname, int zip_fd, int tar_fd, struct zip_directory *dir_entry)
+{
+	int i;
+	struct zip_local_file file_entry;
+	unsigned char *buffer; // used for the compressed file data
+	struct tar_posix_header tar_header = {0};
+	struct gz_header header = {0};
+	struct gz_footer footer = {0};
 	buffer = malloc(dir_entry->zip_size);
 
 	// tar headers
@@ -292,29 +458,66 @@ int zip_locate_eocd(int zip_fd, struct zip_eocd *zip_footer)
 
 int main(int argc, char **argv)
 {
-	int i;
+	int i, j;
+	unsigned char *inname[2];
 	int fd[2];
 	uint32_t offset = 0;
 	unsigned char fname[512];
+	unsigned char *zip_fname, *tar_fname;
+	uint8_t method = BH_MODE_MAKE_TAR;
 
-	if (argc < 3)
+	for (i=1, j=0; i < argc; i++)
 	{
-		printf("You are not argumentative enough to use this program.\n");
-		exit(1);
+		if (argv[i][0] == '-')
+		{
+			switch (argv[i][1])
+			{
+				case BH_MODE_MAKE_TAR: // create tarball
+				case BH_MODE_MAKE_TGZ: // create tar.gz
+				case BH_MODE_EXTRACT:  // Extract to gz
+					method = argv[i][1];
+					break;
+				default:
+					break;
+			}
+		}
+		else
+		{
+			if (j < 2)
+			{
+				inname[j] = argv[i];
+				j++;
+			}
+		}
 	}
 
-	fd[0] = open(argv[1], O_RDONLY, 0);
+	fd[0] = open(inname[0], O_RDONLY, 0);
 	if (fd[0] == -1)
 	{
 		printf("A zip file is required.\n");
+		usage();
 		exit(1);
 	}
 
-	fd[1] = open(argv[2], O_WRONLY | O_CREAT, 0);
-	if (fd[1] == -1)
+	switch (method)
 	{
-		printf("Could not save tar file.\n");
-		exit(1);
+		case BH_MODE_MAKE_TAR:
+		case BH_MODE_MAKE_TGZ:
+			fd[1] = open(inname[1], O_WRONLY | O_CREAT, 0);
+			if (fd[1] == -1)
+			{
+				printf("Could not save tar file.\n");
+				usage();
+				exit(1);
+			}
+			break;
+		case BH_MODE_EXTRACT:
+			break;
+		default:
+			write(1, "You are not argumentative enough to use this program.\n", 54);
+			usage();
+			exit(1);
+			break;
 	}
 
 	struct zip_directory zip_dir = {0};
@@ -325,6 +528,7 @@ int main(int argc, char **argv)
 	if (offset == -1)
 	{
 		printf("This does not appear to be a zip file.\n");
+		usage();
 		exit(1);
 	}
 
@@ -339,21 +543,39 @@ int main(int argc, char **argv)
 		{
 			read(fd[0], &fname, zip_dir.fname_len);
 
+			write(1, fname, zip_dir.fname_len);
 			if (zip_dir.compression == ZIP_ALG_DEFLATE)
 			{
 				fname[zip_dir.fname_len+0] = '.';
 				fname[zip_dir.fname_len+1] = 'g';
 				fname[zip_dir.fname_len+2] = 'z';
 				fname[zip_dir.fname_len+3] = 0;
+				write(1, ".gz\n", 4);
 			}
 			else if (zip_dir.compression == ZIP_ALG_STORE)
 			{
 				fname[zip_dir.fname_len] = 0;
+				write(1, "\n", 1);
 			}
-			write(1, fname, zip_dir.fname_len);
-			write(1, ".gz\n", 4);
 
-			tar_write(fname, fd[0], fd[1], &zip_dir);
+			switch (method)
+			{
+				case BH_MODE_MAKE_TAR:
+					tar_write(fname, fd[0], fd[1], &zip_dir);
+					break;
+				case BH_MODE_EXTRACT:
+					gz_create(fname, fd[0], &zip_dir);
+					break;
+				case BH_MODE_MAKE_TGZ:
+					tgz_write(fname, fd[0], fd[1], &zip_dir);
+					break;
+				default:
+					usage();
+					exit(1);
+					break;
+			}
+			// return to previous position
+//			lseek(fd[0], pos, SEEK_SET);
 			// -------------------------------
 		}
 		lseek(fd[0], zip_dir.extra_len + zip_dir.comment_len, SEEK_CUR);
